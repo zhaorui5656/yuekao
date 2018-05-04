2660b5c912f570848cf54a4c2a15fca06e091a46
zhaorui <rui1550826592@163.com>
2018-05-04 08:26:39 +0800

    再次

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,14 @@
 var gulp = require("gulp");
 var server = require("gulp-webserver");
+
+gulp.task("default", function() {
+    gulp.src("src")
+        .pipe(server({
+            port: 3356,
+            open: true,
+            livereload: true,
+            middleware: function(req, res, next) {
+                next();
+            }
+        }))
+})
